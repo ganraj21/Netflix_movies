780de1ab79494e73bdd617004ac97a09a8186218
Ganesh Ghadge <89139437+ganesh6465@users.noreply.github.com>
2022-07-29 21:50:49 +0530

    use props in different way

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Titlename from './Titlename'
-import Datacomponent from './Datacomponent'
+import Sdata from './Sdata'
+import Card from './Card'
 
 ReactDOM.render(
   <>
     <Titlename />
-    <Datacomponent />
+    {Sdata.map(function ncard(val) {
+      return (
+        <Card
+          imgsrc={val.imgsrc}
+          title={val.title}
+          sname={val.sname}
+          link={val.link}
+        />
+      )
+    })}
   </>,
   document.getElementById('root'),
 )
